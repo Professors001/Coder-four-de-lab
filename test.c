8fd14ab8c0f5c67168ff2405280030ab299ac4de
iDiot-PC <b.aphisit2112@gmail.com>
2023-02-02 21:19:12 +0700

    2/2/2023|21:19

--- test.c
+++ test.c
@@ -1,31 +1,35 @@
 #include <stdio.h>
 
-int remove_vowel(char *str)
+void stringcat(char src[], char dest[])
 {
-    char str2[80];
-    int count = 0, i = 0;
-    for(i = 0; str[i] != '\0'; i++) {
-        char x = str[i];
-        if (x == 'A' || x == 'a' || x == 'E' || x == 'e' || x == 'I' || x == 'i' || x == 'O' || x == 'o' || x == 'U' || x == 'u'){
-            continue;
-        }
-    str2[count] = x;
-    count += 1;
+    char temp[100];
+    int i = 0, c1 = 0, c2 = 0, max = 0;
+    for(c1 = 0; src[c1] != '\0'; c1++);
+    for(c2 = 0; dest[c2] != '\0'; c2++);
+    max = c1 + c2;
+    for(i = 0; i < max; i++) {
+        if(i < c2)
+            temp[i] = dest[i];
+        else
+            temp[i] = src[i-c2];
     }
-    for(i = 0; i <= count; i++) {
-        str[i] = str2[i];
+    temp[max] = '\0';
+    for(i = 0; i < max; i++) {
+        dest[i] = temp[i];
     }
-    str[count] = '\0';
+    dest[max] = '\0';
 }
 
 int main()
-{  char str[80];
+{
+  char in1[100],in2[100];
 
-  printf(" Input: ");
-  gets(str);
-
-  remove_vowel(str);
-  printf("Output: %s\n",str);
-
+  printf("Input 1: ");
+  gets(in1);  /* read a line of characters from the input to "in1" variable */
+  printf("Input 2: ");
+  gets(in2);  /* read another line of characters from the input to "in2" variable */
+  stringcat(in1,in2);
+  printf(" Output: ");
+  printf("%s\n",in2);
   return 0;
 }

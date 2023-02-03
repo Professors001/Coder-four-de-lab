781fdc818fab469699a3d94444fff9fb8fc4b6dd
iDiot-PC <b.aphisit2112@gmail.com>
2023-02-03 11:33:41 +0700

    3/2/2023|11:33

--- week7/lab05.c
+++ week7/lab05.c
@@ -6,7 +6,7 @@
 char* stoupper(const char *str) {
     int len = 0;
     for(; str[len] != '\0'; len++);
-    char *new_str = (char*) malloc(100);
+    char *new_str = (char*) malloc(sizeof(int) * 20);
     for(int i = 0; i < len; i++) {
         new_str[i] = toupper(str[i]);
     }

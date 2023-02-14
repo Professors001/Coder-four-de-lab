33a31da093ecdfb479f541a4cfbb6f6557db5923
iDiot-PC <b.aphisit2112@gmail.com>
2023-02-14 23:12:34 +0700

    14/2/2023|23:12

--- week8/lab06.c
+++ week8/lab06.c
@@ -6,20 +6,41 @@ struct complex {
 
 struct complex add_complex(struct complex C, struct complex D)
 {
-    struct complex addcom;
-    addcom.a = C.a + C.b;
-    addcom.a = C.a + C.b;
-    return addcom;
+    struct complex addcomp;
+    addcomp.a = C.a + D.a;
+    addcomp.b = C.b + D.b;
+    return addcomp;
 }
 
-float multiply_complex()
+struct complex multiply_complex(struct complex C, struct complex D)
 {
-
+    struct complex mulcomp;
+    mulcomp.a = ((C.a * D.a) - (C.b * D.b));
+    mulcomp.b = ((C.a * D.b) + (C.b * D.a));
+    return mulcomp; 
 }
 
-void print_complex()
+void print_complex(const char str[], struct complex x)
 {
-
+    printf("%s", str);
+    if (!x.a) {
+        if (!x.b) {
+            printf("%.1f\n", 0.0);
+        }
+        else {
+            printf("%.1fi\n", x.b);
+        }
+    }   
+    else if (!x.b) {
+        printf("%.1f\n", x.a);
+    } 
+    else {
+        if (x.b < 0)
+            printf("%.1f - %.1fi\n",x.a, -x.b);
+        else
+            printf("%.1f + %.1fi\n",x.a, x.b);
+    }
+    //thank you this if else from tdet_tent_007_2547
 }
 
 int main()
@@ -33,7 +54,7 @@ int main()
     
     add_complex(C, D);
     E = add_complex(C, D);
-    // F = multiply_complex(C, D);
+    F = multiply_complex(C, D);
 
     print_complex("C + D = ", E);
     print_complex("C x D = ", F);

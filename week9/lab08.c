003ed97a7618ea4a0f38cb878e74bfac02e91d87
iDiot-PC <b.aphisit2112@gmail.com>
2023-02-20 04:12:01 +0700

    20/2/2023|04:11

--- week9/lab08.c
+++ week9/lab08.c
@@ -1,15 +1,3 @@
-//file
-// 4
-// 20170401,12:54:00,T Shirt L,1,120.00
-// 20170401,12:56:32,Boxer,2,90.00
-// 20170401,13:12:34,T Shirt L,2,200.00
-// 20170401,13:12:34,Boxer,2,98.00
-// 20170401,14:29:17,T Shirt M,4,480.00
-// 20170401,16:39:27,Shorts M,1,120.50
-// 20170401,17:12:34,T Shirt L,3,350.00
-
-
-
 #include <stdio.h>
 #include <string.h>
 
@@ -32,31 +20,33 @@ void printStock(Product stock[], int nStock)
 void reportStock(char *filename) {
     FILE *f_PTR = fopen(filename, "r");
 
-    int size_array, i, amount, date, n = 0;
-    char txt[500], trash1[9], trash2[9], name[51];
-    double p;
-    fscanf(f_PTR, "%d", &size_array);
-    Product Sell[size_array];
-    fgets(txt, 500, f_PTR); //read 1st line before
-    while(!feof(f_PTR)) {
-        fgets(txt, 500, f_PTR);
-        sscanf(txt, "%8s,%8s,%30[^,],%d,%lf", trash1, trash2, name, &amount, &p);
-        printf("%s %d %lf\n", name, amount, p);
-        for(i = 0; i < size_array; i++) {
-            if(!(strcmp(name, Sell[i].name))) {
-                Sell[i].amount += amount;
-                Sell[i].totalCost += p;
+    char txt[64], name[31];
+    int size_array, i, amount, n = 0, key;
+    double price;
+    fgets(txt, 64, f_PTR);
+    sscanf(txt, "%d", &size_array);
+    Product stock[size_array];
+    while(fgets(txt, 64, f_PTR)) {
+        memset(name, 0, sizeof(name));
+        sscanf(txt, "%[^,],%[^,],%30[^,],%d,%lf", name, name, name, &amount, &price);
+        key = 0;
+        for(i = 0; i < n; i++) {
+            if(!(strcmp(name, stock[i].name))) {
+                stock[i].amount += amount;
+                stock[i].totalCost += price;
+                key = 1;
                 break;
             }
-            strcpy(Sell[n].name, name);
-            Sell[n].amount = amount;
-            Sell[n].totalCost = p; //FUCK YOU BITCH
+        }
+        if(!key) {
+            strcpy(stock[i].name, name);
+            stock[i].amount = amount;
+            stock[i].totalCost = price;
             n++;
         }
     }
-    printStock(Sell, size_array);
+    printStock(stock, size_array);
 }
-
 
 int main()
 {

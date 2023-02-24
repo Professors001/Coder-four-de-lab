0cd701f7291d88a04b6a39b2f3e28d9d8c50649c
aphisit prasertvesyakorn <b.aphisit2112@gmail.com>
2023-02-24 22:58:05 +0700

    24/2/2023|22:57

--- test.c
+++ test.c
@@ -1,74 +1,48 @@
 #include <stdio.h>
 #include <stdlib.h>
 
-typedef struct Node_ {
-    int value;
-    struct Node_ *next;
+#define SIZE  97
+
+typedef struct nodeType {
+  int   item;
+  struct nodeType* next;
 } Node;
 
 void printNode(Node *start) {
-    Node *current = start;
-    printf("[ ");
-    while(current != NULL) {
-        printf("%d ", current->value);
-        current = current->next;        
+    for(int i = 0; i < SIZE; i++) {
+        Node *current = start;
+        printf("KEY% 3d: ", i);
+        while(current != NULL) {
+            if((current->item % SIZE) == i) {
+                printf("%d ", current->item);
+            }
+            current = current->next;
+        }
+        puts("");
     }
-    printf("]\n");
 }
 
-void insertNode(Node **start, int value) {
+void addNode(Node **start, int value) {
+    //it work now dont touch it
     Node *temp = (Node*)malloc(sizeof(Node));
-    temp->value = value;
-    if(*start == NULL) {
-        temp->next = *start;
-        *start = temp;
-    } else if((*start)->value > value) {
-        temp->next = *start;
-        *start = temp;
-    } else {
-        Node *current = *start;
-        while (current->next != NULL && current->next->value < value) {
-            current = current->next;
-        }
-        temp->next = current->next;
-        current->next = temp;
-    }
-}
-
-void deleteNode(Node **start, int value) {
-    Node *current = *start, *prev = NULL;
-    while(current != NULL) {
-        if(current->value == value) {
-            if(current == *start)
-                *start = current->next;
-            else
-                prev->next = current->next;
-            Node *temp = current;
-            current = current->next;
-            free(temp);
-        }
-        else {
-            prev = current;
-            current = current->next;
-        }
-    }
+    temp->item = value; temp->next = *start;
+    *start = temp;
 }
 
 int main() {
     Node *start = NULL;
-    int check = 1, value;
-    char christ, array[32];
+    int check = 1, value, i = 0;
+    
+    char christ, array[16];
     while(check) {
         christ = '0';
         printf("input> ");
-        fgets(array,32,stdin);
+        fgets(array,16,stdin);
         sscanf(array,"%c %d",&christ,&value);
-        if (christ == 'i')
-            insertNode(&start, value);
+        if (christ == 'a')
+            addNode(&start, value);
         else if (christ == 'p')
             printNode(start);
-        else if (christ == 'd')
-            deleteNode(&start, value);
         else if (christ == 'q')
             check = 0;
         
